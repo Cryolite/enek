860eeb6a9ece9fce4a19e21f1918c589a5378e78
Cryolite <cryolite.indigo@gmail.com>
2018-06-10 03:23:06 +0900

    Fix paths in shell scripts.

--- docker/devel/build.sh
+++ docker/devel/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-. ../../util/common.sh
+THIS_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+. "$THIS_DIR/../../util/common.sh"
 
 if type -t timedatectl >&/dev/null; then
     timezone="$(timedatectl | grep -F 'Time zone:' | sed -e 's/.*Time zone:[[:space:]]\{1,\}\([^[:space:]]\{1,\}\)[[:space:]]\{1,\}.*/\1/')"
@@ -12,9 +13,9 @@ else
 fi
 timezone_area="$(echo "$timezone" | sed -e 's@/.*@@')"
 timezone_location="$(echo "$timezone" | sed -e 's@.*/@@')"
-cp preseed.txt.orig preseed.txt
-sed -i -e "s@\(tzdata[[:space:]]\{1,\}tzdata/Areas[[:space:]]\{1,\}select\{1,\}[[:space:]]\{1,\}\).*@\1$timezone_area@" preseed.txt
-sed -i -e "s@\(tzdata\s\{1,\}tzdata/Zones/\)[^[:space:]]\{1,\}\([[:space:]]\{1,\}select\{1,\}[[:space:]]\{1,\}\).*@\1$timezone_area\2$timezone_location@" preseed.txt
+cp "$THIS_DIR/preseed.txt.orig" "$THIS_DIR/preseed.txt"
+sed -i -e "s@\(tzdata[[:space:]]\{1,\}tzdata/Areas[[:space:]]\{1,\}select\{1,\}[[:space:]]\{1,\}\).*@\1$timezone_area@" "$THIS_DIR/preseed.txt"
+sed -i -e "s@\(tzdata\s\{1,\}tzdata/Zones/\)[^[:space:]]\{1,\}\([[:space:]]\{1,\}select\{1,\}[[:space:]]\{1,\}\).*@\1$timezone_area\2$timezone_location@" "$THIS_DIR/preseed.txt"
 
-"../build/build.sh"
+"$ENEK_ROOT_DIR/docker/build/build.sh"
 (cd "$THIS_DIR" && docker build -t cryolite/enek-devel .)
