c5bdf1d2ab50ce086e44285a5b918ce6cef2cb15
Cryolite <cryolite.indigo@gmail.com>
2018-06-11 02:30:36 +0900

    Add a new test script.

--- util/common.sh
+++ util/common.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 ENEK_ROOT_DIR="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/..)"
-PS4='+$(realpath --relative-to="$ENEK_ROOT_DIR" "$(realpath "${BASH_SOURCE[0]}")"):$LINENO: '
+SOURCE_PATH="$(realpath "${BASH_SOURCE[1]}")"
+SOURCE_PATH_RELATIVE="$(realpath --relative-to="$ENEK_ROOT_DIR" "$SOURCE_PATH")"
+PS4='+$SOURCE_PATH_RELATIVE:$LINENO: '
 if test -t 1 && tput setaf 1 >&/dev/null; then
     if test "$(tput colors)" == 256; then
         PS4='$(tput setaf 10)'$PS4'$(tput sgr0)'
