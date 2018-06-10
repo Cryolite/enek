907c053b3983369544dd6b9b0d4a0bd2bf11b677
Cryolite <cryolite.indigo@gmail.com>
2018-06-11 04:35:13 +0900

    Replace dependency on realpath with Python script.

--- util/common.sh
+++ util/common.sh
@@ -1,8 +1,36 @@
 #!/usr/bin/env bash
 
+function realpath ()
+{
+  local script="import os.path; print(os.path.realpath('$1'))"
+  if type -t python >/dev/null; then
+    python -c "$script"
+  elif type -t python2 >/dev/null; then
+    python2 -c "$script"
+  elif type -t python3 >/dev/null; then
+    python3 -c "$script"
+  else
+    realpath "$1"
+  fi
+}
+
+function relpath ()
+{
+  local script="import os.path; print(os.path.relpath('$1', '${2:-$PWD}'))"
+  if type -t python >/dev/null; then
+    python -c "$script"
+  elif type -t python2 >/dev/null; then
+    python2 -c "$script"
+  elif type -t python3 >/dev/null; then
+    python3 -c "$script"
+  else
+    realpath --relative-to="${2:-$PWD}" "$1"
+  fi
+}
+
 ENEK_ROOT_DIR="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/..)"
 SOURCE_PATH="$(realpath "${BASH_SOURCE[1]}")"
-SOURCE_PATH_RELATIVE="$(realpath --relative-to="$ENEK_ROOT_DIR" "$SOURCE_PATH")"
+SOURCE_PATH_RELATIVE="$(relpath "$SOURCE_PATH" "$ENEK_ROOT_DIR")"
 PS4='+$SOURCE_PATH_RELATIVE:$LINENO: '
 if test -t 1 && tput setaf 1 >&/dev/null; then
     if test "$(tput colors)" == 256; then
