55a7d547df053d4e0185485d1de956ff149214ab
Cryolite <cryolite.indigo@gmail.com>
2018-06-11 05:50:03 +0900

    Add build script for Travis CI.

---
+++ script/cibuild.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SOURCE_DIR="$(dirname "$(readlink -e "${BASH_SOURCE[0]}")")"
+. "$SOURCE_DIR/../util/common.sh"
+
+"$ENEK_ROOT_DIR/script/test.sh" --build-dir=/work/enek-build --concurrency=auto --debug --enable-coverage
+
+declare -a coveralls_options
+if test "$TRAVIS_PULL_REQUEST" != false; then
+  coveralls_options+=(--service-pull-request="$TRAVIS_PULL_REQUEST")
+fi
+"$ENEK_ROOT_DIR/util/coveralls.py" --source-root="$ENEK_ROOT_DIR" --build-root=/work/enek-build --with-gcov=/usr/local/bin/gcov --clear --service-name=travis-ci --service-number="$TRAVIS_JOB_NUMBER" --service-job-id="$TRAVIS_JOB_ID" "${coveralls_options[@]}"
