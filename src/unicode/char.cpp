57439d4669a02ba7f563696e598677e892358697
Cryolite <cryolite.indigo@gmail.com>
2018-06-17 05:02:15 +0900

    Add support for East Asian Width.

--- src/unicode/char.cpp
+++ src/unicode/char.cpp
@@ -1,7 +1,11 @@
 #include <enek/unicode/char.hpp>
-
+#include <enek/util/throw.hpp>
 #include <enek/util/assert.hpp>
 #include <unicode/uchar.h>
+#include <unicode/umachine.h>
+#include <ostream>
+#include <stdexcept>
+#include <cstdint>
 
 
 namespace Enek::Unicode{
@@ -20,5 +24,66 @@ bool isOtherSurrogate(char32_t c) noexcept
 }
 
 } // namespace GeneralCategory
+
+std::ostream &operator<<(std::ostream &os, EastAsianWidth eaw)
+{
+  switch (eaw) {
+  case EastAsianWidth::fullwidth:
+    os << "East Asian Fullwidth";
+    break;
+  case EastAsianWidth::halfwidth:
+    os << "East Asian Halfwidth";
+    break;
+  case EastAsianWidth::wide:
+    os << "East Asian Wide";
+    break;
+  case EastAsianWidth::narrow:
+    os << "East Asian Narrow";
+    break;
+  case EastAsianWidth::ambiguous:
+    os << "East Asian Ambiguous";
+    break;
+  case EastAsianWidth::neutral:
+    os << "East Asian Neutral";
+    break;
+  default:
+    ENEK_THROW<std::logic_error>("");
+  }
+  return os;
+}
+
+EastAsianWidth getEastAsianWidth(char32_t c)
+{
+  EastAsianWidth result;
+  {
+    std::int32_t const v = u_getIntPropertyValue(
+      static_cast<UChar32>(c), UCHAR_EAST_ASIAN_WIDTH);
+    switch (static_cast<UEastAsianWidth>(v)) {
+    case UEastAsianWidth::U_EA_NEUTRAL:
+      result = EastAsianWidth::neutral;
+      break;
+    case UEastAsianWidth::U_EA_AMBIGUOUS:
+      result = EastAsianWidth::ambiguous;
+      break;
+    case UEastAsianWidth::U_EA_HALFWIDTH:
+      result = EastAsianWidth::halfwidth;
+      break;
+    case UEastAsianWidth::U_EA_FULLWIDTH:
+      result = EastAsianWidth::fullwidth;
+      break;
+    case UEastAsianWidth::U_EA_NARROW:
+      result = EastAsianWidth::narrow;
+      break;
+    case UEastAsianWidth::U_EA_WIDE:
+      result = EastAsianWidth::wide;
+      break;
+    default:
+      ENEK_THROW<std::logic_error>(
+        "`u_getIntPropertyValue(c, UCHAR_EAST_ASIAN_WIDTH)' returns an "
+        "unknown value.");
+    }
+  }
+  return result;
+}
 
 } // namespace Enek::Unicode
--- test/unit/unicode/char.cpp
+++ test/unit/unicode/char.cpp
@@ -1,5 +1,6 @@
 #include <enek/unicode/char.hpp>
 #include <gtest/gtest.h>
+#include <sstream>
 
 
 TEST(UnicodeCharTest, testGetCharMaxValue)
@@ -14,4 +15,41 @@ TEST(UnicodeCharTest, testIsOtherSurrogate)
   EXPECT_TRUE(isOtherSurrogate(U'\xD800'));
   EXPECT_TRUE(isOtherSurrogate(U'\xDFFF'));
   EXPECT_FALSE(isOtherSurrogate(U'\uE000'));
+}
+
+TEST(UnicodeCharTest, testEastAsianWidthOStream)
+{
+  using Enek::Unicode::EastAsianWidth;
+  using Enek::Unicode::getEastAsianWidth;
+  std::ostringstream oss;
+  oss << EastAsianWidth::fullwidth;
+  EXPECT_STREQ("East Asian Fullwidth", oss.str().c_str());
+  oss = std::ostringstream();
+  oss << EastAsianWidth::halfwidth;
+  EXPECT_STREQ("East Asian Halfwidth", oss.str().c_str());
+  oss = std::ostringstream();
+  oss << EastAsianWidth::wide;
+  EXPECT_STREQ("East Asian Wide", oss.str().c_str());
+  oss = std::ostringstream();
+  oss << EastAsianWidth::narrow;
+  EXPECT_STREQ("East Asian Narrow", oss.str().c_str());
+  oss = std::ostringstream();
+  oss << EastAsianWidth::ambiguous;
+  EXPECT_STREQ("East Asian Ambiguous", oss.str().c_str());
+  oss = std::ostringstream();
+  oss << EastAsianWidth::neutral;
+  EXPECT_STREQ("East Asian Neutral", oss.str().c_str());
+}
+
+TEST(UnicodeCharTest, testGetEastAsianWidth)
+{
+  using Enek::Unicode::EastAsianWidth;
+  using Enek::Unicode::getEastAsianWidth;
+  char32_t c = 32;
+  EXPECT_EQ(EastAsianWidth::fullwidth, getEastAsianWidth(U'\u3000'));
+  EXPECT_EQ(EastAsianWidth::halfwidth, getEastAsianWidth(U'\u20A9'));
+  EXPECT_EQ(EastAsianWidth::wide, getEastAsianWidth(U'\u1100'));
+  EXPECT_EQ(EastAsianWidth::narrow, getEastAsianWidth(U'\u0020'));
+  EXPECT_EQ(EastAsianWidth::ambiguous, getEastAsianWidth(U'\u0101'));
+  EXPECT_EQ(EastAsianWidth::neutral, getEastAsianWidth(U'\u007F'));
 }
